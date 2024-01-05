01038d1e2942e8b335e4c0109599e94ed2d1de4d
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2024-01-04 23:32:15 -0300

    refactor: Refactor logger interface and implementation

--- logger/logger.go
+++ logger/logger.go
@@ -18,44 +18,14 @@ const (
 	FATAL
 )
 
-type Logger interface {
-	Info(message string)
-	Success(message string)
-	Warning(message string)
-	Error(message string)
-	Panic(message string)
-	Fatal(message string)
-}
-
-type logger struct{}
-
-func NewLogger() Logger {
-	return &logger{}
-}
-
-func (l *logger) Info(message string) {
-	l.log(INFO, message)
-}
-
-func (l *logger) Success(message string) {
-	l.log(SUCCESS, message)
-}
-
-func (l *logger) Warning(message string) {
-	l.log(WARNING, message)
-}
-
-func (l *logger) Error(message string) {
-	l.log(ERROR, message)
-}
-
-func (l *logger) Panic(message string) {
-	l.log(PANIC, message)
-}
-
-func (l *logger) Fatal(message string) {
-	l.log(FATAL, message)
-}
+var (
+	Info    = logFunc(INFO)
+	Success = logFunc(SUCCESS)
+	Warning = logFunc(WARNING)
+	Error   = logFunc(ERROR)
+	Panic   = logFunc(PANIC)
+	Fatal   = logFunc(FATAL)
+)
 
 /*
 Log imprime uma mensagem no console com o nível de log informado.
@@ -80,25 +50,27 @@ Log imprime uma mensagem no console com o nível de log informado.
 
 @struct logger
 */
-func (l *logger) log(level Level, message string) {
-	_, fn, line, _ := runtime.Caller(2)
-	threadID := fmt.Sprintf("%d", time.Now().UnixNano())
-	logDetails := fmt.Sprintf("| %s | %s:%d", threadID, fn, line)
+func logFunc(level Level) func(message string) {
+	return func(message string) {
+		_, fn, line, _ := runtime.Caller(1)
+		threadID := fmt.Sprintf("%d", time.Now().UnixNano())
+		logDetails := fmt.Sprintf("| %s | %s:%d", threadID, fn, line)
 
-	switch level {
-	case INFO:
-		log.Printf("\033[34m%s | INFO: %s\033[0m", logDetails, message)
-	case SUCCESS:
-		log.Printf("\033[32m%s | SUCCESS: %s\033[0m", logDetails, message)
-	case WARNING:
-		log.Printf("\033[33m%s | WARNING: %s\033[0m", logDetails, message)
-	case ERROR:
-		log.Printf("\033[31m%s | ERROR: %s\033[0m", logDetails, message)
-	case PANIC:
-		log.Printf("\033[35m%s | PANIC: %s\033[0m", logDetails, message)
-	case FATAL:
-		log.Printf("\033[2m%s | FATAL: %s\033[0m", logDetails, message)
-	default:
-		log.Printf("%s | %s", logDetails, message)
+		switch level {
+		case INFO:
+			log.Printf("\033[34m%s | INFO: %s\033[0m", logDetails, message)
+		case SUCCESS:
+			log.Printf("\033[32m%s | SUCCESS: %s\033[0m", logDetails, message)
+		case WARNING:
+			log.Printf("\033[33m%s | WARNING: %s\033[0m", logDetails, message)
+		case ERROR:
+			log.Printf("\033[31m%s | ERROR: %s\033[0m", logDetails, message)
+		case PANIC:
+			log.Printf("\033[35m%s | PANIC: %s\033[0m", logDetails, message)
+		case FATAL:
+			log.Printf("\033[2m%s | FATAL: %s\033[0m", logDetails, message)
+		default:
+			log.Printf("%s | %s", logDetails, message)
+		}
 	}
 }
